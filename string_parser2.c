eea1e66f5e5f671222d6218efd046c5e5e0f17c8
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-06-16 18:09:16 +0200

    string_parser2.c file

--- string_parser2.c
+++ string_parser2.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - determine the length of a string
- * @s: pointer to the string to check
+ * @s: pointer to the string to check.
  * Return: length of the string check
 */
 int _strlen(char *s)
